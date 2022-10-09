b6ff4b401da42393a333cf88ae4c520d88da8500
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-10-09 15:58:01 -0300

    `docs`: add storybook to document frontend

---
+++ src/stories/Home.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
+import { Home } from '../pages/Home'
+
+export default {
+  title: 'Pages/Home',
+  component: Home,
+  decorators: [
+    (Story) => {
+      return <BrowserRouter>{Story()}</BrowserRouter>
+    },
+  ],
+} as Meta
+
+export const Default: StoryObj = {}
